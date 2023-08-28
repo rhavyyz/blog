8359efc708fb12adfff78ec9edeb9e41c93daf3a
Rhavy <rhavyf1411@gmail.com>
2023-08-27 22:19:56 -0300

    this time the real first commit

---
+++ blog-next/src/app/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type Post = {
+  title : string,
+  description: string,
+  content : string
+}
+
+interface Resp<type>
+{
+  id: number,
+  attributes : type
+}
+
+export default async function Home() {
+  
+  const res : Resp<Post>[]= (await(await fetch( "http://localhost:1337/api/posts", {cache : "no-store"})).json())["data"];
+  
+
+  return (
+    <div>
+      { res[0].attributes.title } {res.length}
+    </div>
+  )
+}
